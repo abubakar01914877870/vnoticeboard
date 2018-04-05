e5d15698741b50c3f86db8f3057c6d1260879162
Personal <Personal@abubakar>
2018-04-05 15:39:27 +0600

    First commit

---
+++ vnotice/src/main/java/com/blackartstudio/vnoticeboard/VnoticeApplication.java
@@ -0,0 +1,12 @@
+package com.blackartstudio.vnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnoticeApplication.class, args);
+	}
+}

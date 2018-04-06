929077d36cf20788609ee7ed9784c147e4de8e2d
Personal <Personal@abubakar>
2018-04-06 19:55:21 +0600

    add board repository

--- vnotice/src/main/java/com/blackartstudio/vnoticeboard/controller/HomeController.java
+++ vnotice/src/main/java/com/blackartstudio/vnoticeboard/controller/HomeController.java
@@ -2,18 +2,25 @@ package com.blackartstudio.vnoticeboard.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.blackartstudio.vnoticeboard.entity.Board;
+import com.blackartstudio.vnoticeboard.repository.BoardRepository;
 
 @Controller
 public class HomeController {
 	
 	public static final Logger LOGGER=LoggerFactory.getLogger(HomeController.class);
 	
+	
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String showHome() {
 		LOGGER.debug("Rendering home page");
+		
 		return "home";
 	}
 }
--- vnotice/src/test/java/com/blackartstudio/vnoticeboard/VnoticeApplicationTests.java
+++ vnotice/src/test/java/com/blackartstudio/vnoticeboard/VnoticeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.blackartstudio.vnoticeboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
